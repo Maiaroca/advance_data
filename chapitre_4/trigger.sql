a41177d09a418c36ad0ab7683e04674ff737e78d
Ficus22 <130400795+Ficus22@users.noreply.github.com>
2024-12-16 18:12:23 +0100

    Trigger modif

--- chapitre_4/trigger.sql
+++ chapitre_4/trigger.sql
@@ -1,6 +1,6 @@
 -- Création d'une table de log si elle n'existe pas déjà
 BEGIN
-    EXECUTE IMMEDIATE 'CREATE TABLE loader_user.AuditLog (
+    EXECUTE IMMEDIATE 'CREATE TABLE admin_user.AuditLog (
         log_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         table_name VARCHAR2(100),
         operation VARCHAR2(20),
@@ -16,40 +16,40 @@ END;
 
 -- Trigger pour la table MoviesGenres
 CREATE OR REPLACE TRIGGER trg_moviesgenres_log
-AFTER INSERT OR UPDATE OR DELETE ON loader_user.MoviesGenres
+AFTER INSERT OR UPDATE OR DELETE ON admin_user.MoviesGenres
 FOR EACH ROW
 BEGIN
-    INSERT INTO loader_user.AuditLog (table_name, operation, timestamp)
+    INSERT INTO admin_user.AuditLog (table_name, operation, timestamp)
     VALUES ('MoviesGenres', ORA_SYSEVENT, SYSDATE);
 END;
 /
 
 -- Trigger pour la table MoviesCountries
 CREATE OR REPLACE TRIGGER trg_moviescountries_log
-AFTER INSERT OR UPDATE OR DELETE ON loader_user.MoviesCountries
+AFTER INSERT OR UPDATE OR DELETE ON admin_user.MoviesCountries
 FOR EACH ROW
 BEGIN
-    INSERT INTO loader_user.AuditLog (table_name, operation, timestamp)
+    INSERT INTO admin_user.AuditLog (table_name, operation, timestamp)
     VALUES ('MoviesCountries', ORA_SYSEVENT, SYSDATE);
 END;
 /
 
 -- Trigger pour la table MoviesDirectors
 CREATE OR REPLACE TRIGGER trg_moviesdirectors_log
-AFTER INSERT OR UPDATE OR DELETE ON loader_user.MoviesDirectors
+AFTER INSERT OR UPDATE OR DELETE ON admin_user.MoviesDirectors
 FOR EACH ROW
 BEGIN
-    INSERT INTO loader_user.AuditLog (table_name, operation, timestamp)
+    INSERT INTO admin_user.AuditLog (table_name, operation, timestamp)
     VALUES ('MoviesDirectors', ORA_SYSEVENT, SYSDATE);
 END;
 /
 
 -- Trigger pour la table MoviesCast
 CREATE OR REPLACE TRIGGER trg_moviescast_log
-AFTER INSERT OR UPDATE OR DELETE ON loader_user.MoviesCast
+AFTER INSERT OR UPDATE OR DELETE ON admin_user.MoviesCast
 FOR EACH ROW
 BEGIN
-    INSERT INTO loader_user.AuditLog (table_name, operation, timestamp)
+    INSERT INTO admin_user.AuditLog (table_name, operation, timestamp)
     VALUES ('MoviesCast', ORA_SYSEVENT, SYSDATE);
 END;
 /
