07573726a4116a38bc484e012cb55ef913a25a6d
Ficus22 <130400795+Ficus22@users.noreply.github.com>
2024-12-16 12:24:34 +0100

    Suppression dossier doublons

--- Advance_database-main/chapitre_2/security_config.sql
+++
@@ -1,9 +0,0 @@
--- Configuration du profil par défaut
-ALTER PROFILE DEFAULT LIMIT PASSWORD_VERIFY_FUNCTION NULL;
-
--- Activer l'audit
-AUDIT SELECT ON loader_user.MoviesAndShows BY ACCESS;
-AUDIT INSERT, UPDATE, DELETE ON loader_user.MoviesAndShows BY ACCESS;
-
-AUDIT SELECT ON loader_user.Directors BY ACCESS;
-AUDIT INSERT, UPDATE, DELETE ON loader_user.Directors BY ACCESS;
